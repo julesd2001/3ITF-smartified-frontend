6535a218e4c566426cd4233c10ae25caf376794f
Michiel Hendrickx <michielhendrickx@live.be>
2022-01-19 10:27:21 +0100

    Rechtse sidenav

---
+++ src/app/graph-form/graph-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graph-form',
+  templateUrl: './graph-form.component.html',
+  styleUrls: ['./graph-form.component.scss']
+})
+export class GraphFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
